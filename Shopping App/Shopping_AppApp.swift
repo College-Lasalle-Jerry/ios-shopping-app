64958501913476df4c9c746a9c9801932c5e6943
centinna-l <jerryjoy99@gmail.com>
2024-09-29 15:04:10 -0400

    Initial Commit

---
+++ Shopping App/Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_AppApp.swift
+//  Shopping App
+//
+//  Created by Jerry Joy on 2024-09-29.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
